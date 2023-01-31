fd0a69e12c9911b5aaf552b2f74bc17bf024e0b8
enyinnaya-joshua <enyinnayajoshua56@gmail.com>
2023-01-31 14:30:09 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header  from './Components/Header';
+import Body from './Components/Body';
+import VercelFooter from './Components/VercelFooter';
+
+function App() {
+  return (
+    <div>
+        <Header />
+        <Body />
+        <VercelFooter />
+    </div>
+  );
+}
+
+export default App;
